edc7903c23a9638537789f254e228cccd9294953
Michael Pedrotti <michael.hscbrasil@gmail.com>
2018-02-01 10:02:16 -0200

    Localization

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -1,24 +1,10 @@
 @extends('layout.app')
 
 @section('content')
- @if (session('status'))
- 
- <div class="box box-primary">
-	 <div class="box-body pad table-responsive">
-		 sdfafds
-		 
-	 </div>
-	 
- </div>
-	<div class="alert alert-success">
-		{{ session('status') }}
-	</div>
-@endif
-
 <div class="box box-primary">
-
 	<div class="box-body">
 		Start creating your amazing application!
+		@lang('Super programa')
 	</div>
 </div>
 @endsection
