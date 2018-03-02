38563dd667d9b0a75a4ab172dda7070d322da0d2
Michael Pedrotti <michael.hscbrasil@gmail.com>
2018-03-02 11:10:30 -0300

    licenses

--- database/seeds/ProductsSeeder.php
+++ database/seeds/ProductsSeeder.php
@@ -6,51 +6,47 @@ class ProductsSeeder extends Seeder {
 
 	public function run() {
 
+		$$datetime = date('Y-m-d H:i:s');
 		
 		\DB::table('products')->insert([
 			[
 				'id' => '1',
 				'name' => 'MailInspector',
 				'version' => '1.5',
-				'created_at' => '2018-02-19 19:15:46',
-				'updated_at' => '2018-02-19 19:15:46',
-				'deleted_at' => '',
+				'created_at' => $datetime,
+				'updated_at' => $datetime,
 				'uid' => 'mli',
 			],
 			[
 				'id' => '2',
 				'name' => 'MailInspector',
 				'version' => '3.0',
-				'created_at' => '2018-02-22 20:08:14',
-				'updated_at' => '2018-02-22 20:08:14',
-				'deleted_at' => '',
+				'created_at' => $datetime,
+				'updated_at' => $datetime,
 				'uid' => 'mli',
 			],
 			[
 				'id' => '3',
 				'name' => 'MailInspector',
 				'version' => '4.0',
-				'created_at' => '2018-02-22 20:08:53',
-				'updated_at' => '2018-02-22 20:08:53',
-				'deleted_at' => '',
+				'created_at' => $datetime,
+				'updated_at' => $datetime,
 				'uid' => 'mli',
 			],
 			[
 				'id' => '4',
 				'name' => 'ISS',
 				'version' => '3.0',
-				'created_at' => '2018-02-23 11:32:58',
-				'updated_at' => '2018-02-23 11:32:58',
-				'deleted_at' => '',
+				'created_at' => $datetime,
+				'updated_at' => $datetime,
 				'uid' => 'iss',
 			],
 			[
 				'id' => '5',
 				'name' => 'ISS',
 				'version' => '4.0',
-				'created_at' => '2018-02-23 11:43:57',
-				'updated_at' => '2018-02-23 11:43:57',
-				'deleted_at' => '',
+				'created_at' => $datetime,
+				'updated_at' => $datetime,
 				'uid' => 'iss',
 			],
 		]);
