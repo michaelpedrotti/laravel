6eff32559cacf3830842c050a950f4a073c69b7e
Michael Pedrotti <michael.hscbrasil@gmail.com>
2018-03-20 18:05:37 -0300

    fix

--- database/seeds/LicenseTypesSeeder.php
+++ database/seeds/LicenseTypesSeeder.php
@@ -6,42 +6,56 @@ class LicenseTypesSeeder extends Seeder {
 
 	public function run() {
 
+		$datetime = date('Y-m-d H:i:s');
+		
 		\DB::table('license_types')->insert([
 			[
 				'id' => 1,
 				'name' => 'Network',
 				'product_id' => 1,
 				'val' => 1,
+				'created_at' => $datetime,
+				'updated_at' => $datetime
 			],
 			[
 				'id' => 2,
 				'name' => 'Enterprise',
 				'product_id' => 1,
 				'val' => 2,
+				'created_at' => $datetime,
+				'updated_at' => $datetime
 			],
 			[
 				'id' => 3,
 				'name' => 'Free',
 				'product_id' => 1,
 				'val' => 3,
+				'created_at' => $datetime,
+				'updated_at' => $datetime
 			],
 			[
 				'id' => 4,
 				'name' => 'Network',
 				'product_id' => 2,
 				'val' => 1,
+				'created_at' => $datetime,
+				'updated_at' => $datetime
 			],
 			[
 				'id' => 5,
 				'name' => 'Enterprise',
 				'product_id' => 2,
 				'val' => 2,
+				'created_at' => $datetime,
+				'updated_at' => $datetime
 			],
 			[
 				'id' => 6,
 				'name' => 'ISP',
 				'product_id' => 2,
 				'val' => 3,
+				'created_at' => $datetime,
+				'updated_at' => $datetime
 			]
 		]);
 	}
